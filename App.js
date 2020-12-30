4e99dfc03775d9da761f8c2e987ef798cc1636e9
Arun Antony <arun.antony@knab.nl>
2020-12-30 19:43:59 +0100

    useState to add list of goals

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [goal, setGoal] = useState('Okay, Lets go!!! :-)');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goadInputHandler = (enteredText) => {
+    setGoal(enteredText);
+  }
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, goal]);
+    setGoal('');
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Okay, Lets go!!!</Text>
+    <View style={styles.container} >
+      <TextInput
+        placeholder="course goal"
+        style={{ borderBottomColor: 'black', borderBottomWidth: 2 }}
+        onChangeText={goadInputHandler}
+        value={goal}
+      />
+      <Button 
+        title="Add above goal to your list" 
+        onPress={addGoalHandler} />
+      {courseGoals.map((goal, i) => <Text key={i}>{goal}</Text>)}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   padding: 40
   },
+  // button: {
+  //   position: 'absolute',
+  //   bottom: 0
+  // }
 });
